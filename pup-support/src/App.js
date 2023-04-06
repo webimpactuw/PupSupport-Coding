f428d44b4cb93fdac548c319dacba379cc0e61e4
Jaela Field <jaelaf@uw.edu>
2023-04-05 19:39:52 -0700

    nav bar progress + related new dependency

--- pup-support/src/App.js
+++ pup-support/src/App.js
@@ -1,9 +1,21 @@
 import './css/App.css';
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home';
+import Resources from './pages/Resources';
 
 function App() {
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/resources' component={Resources} />
+        {/* <Route path='/about' component={About} />
+        <Route path='/merch' component={Merch} />
+        <Route path='donation' component={Donation} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
