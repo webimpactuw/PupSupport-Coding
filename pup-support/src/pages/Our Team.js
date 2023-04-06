a517ae474ca995874a5170fe3ab84aeb6792bf5d
VERAGUBER <VERA2@UW.EDU>
2023-04-05 18:33:52 -0700

    started basic Our Team layout

---
+++ pup-support/src/pages/Our Team.js
@@ -0,0 +1,64 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+/**
+ * Pupsupport Our Team Page
+ */
+function Home() {
+    // Related functions here
+
+    // HTML/React components here to render page
+    return (
+        <div>
+            <Header></Header>
+            <div>
+              <h1>Our Team</h1>
+              <p>Meet the faces behind Pup Support. Our team is made up of innovators, designers, and compassionate members who value the importance of inclusive mental health access.</p>
+              <p>Lorem ipsum dolor sit amet consectetur. Urna morbi fringilla mauris sed. Curabitur fringilla neque dictum ultrices blandit turpis eget.</p>
+            </div>
+            <section>
+              <h2>Executive Team</h2>
+                <div>
+                  <div>
+                    <img src="" alt="Member Name"/>
+                    <p>Name</p>
+                    <p>Position</p>
+                  </div>
+                </div>
+            </section>
+            <section>
+              <h2>Resource & Education Team</h2>
+                <div>
+                  <div>
+                    <img src="" alt="Member Name"/>
+                    <p>Name</p>
+                    <p>Position</p>
+                  </div>
+                </div>
+            </section>
+            <section>
+              <h2>External Team</h2>
+                <div>
+                  <div>
+                    <img src="" alt="Member Name"/>
+                    <p>Name</p>
+                    <p>Position</p>
+                  </div>
+                </div>
+            </section>
+            <section>
+              <h2>Creative Team</h2>
+                <div>
+                  <div>
+                    <img src="" alt="Member Name"/>
+                    <p>Name</p>
+                    <p>Position</p>
+                  </div>
+                </div>
+            </section>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Home;
