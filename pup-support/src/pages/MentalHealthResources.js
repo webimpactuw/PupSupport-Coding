6dd93443aee067c6ab9db115b5c1d89cd612a171
MasonH <mason.hiromoto@gmail.com>
2023-04-11 17:06:19 -0700

    Mental Health Resources Sketch

---
+++ pup-support/src/pages/MentalHealthResources.js
@@ -0,0 +1,59 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+/**
+ * Pupsupport home page
+ */
+function MentalHealthResources() {
+    // Related functions here
+
+    // HTML/React components here to render page
+    return (
+        <div>
+            <Header></Header>
+            <header>
+                <section>
+                    <div>
+                        <h1>Mental Health Resource Links</h1>
+                        <p>Lorem ipsum dolor sit met consectetur.</p>
+                    </div>
+                    <div>
+                        <img src="" alt="Dubs" />
+                    </div>
+                </section>
+            </header>
+            <main>
+                <section>
+                    <h2>Mental Health Resources</h2>
+                    <div>
+                        <div>
+                            <p>Title</p>
+                            <img src="" alt="Dubs" />
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <h2>Additional Resources</h2>
+                    <div>
+                        <div>
+                            <p>Title</p>
+                            <img src="" alt="Dubs" />
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <h2>Our Resources</h2>
+                    <div>
+                        <div>
+                        <p>Title</p>
+                        <img src="" alt="Dubs" />
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default MentalHealthResources;
