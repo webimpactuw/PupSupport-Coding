85b297f6eaf3a7c852c42818ef211c9ee61d987e
eka lotus <samantha_reksosamudra@yahoo.com>
2023-04-11 18:36:55 -0700

    Updated resources (React) page

--- pup-support/src/pages/Resources.js
+++ pup-support/src/pages/Resources.js
@@ -11,6 +11,26 @@ function Resources() {
         <div>
             {/* Sam, replace this with your resources code */}
             <p>This is the resources page</p>
+
+            <section class="left-main">
+                <img src = "/pup-support/illust/pup_tilting.png" alt = "Pup Tilting Pic"/>
+                <h2>Resources</h2>
+
+                <div class="disclaimer">
+                    <p>Diclaimer: We are not licensed or trained professionals. All of the information we share and create
+                    is collected from existing resources, academic papers, and resources provided by professionals.</p>
+                </div>
+            </section>
+
+            {/*add links to other pages */}
+            <section class="resources-links">
+                <ul>
+                    <li><Link to='/mental-health-resource'>Resources Websites</Link></li>
+                    <li><Link to='/videos'>PupSupport YouTube</Link></li>
+                    <li><Link to='/podcast'>PupSupport Podcast</Link></li>
+                </ul>
+            </section>
+
             
             {/* example of how linking will work */}
             <Link to='/videos'>Videos</Link>
