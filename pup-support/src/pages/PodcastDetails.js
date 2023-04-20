fecd81196b5a9eaad946c7b54d4254f00365ef9a
eka lotus <samantha_reksosamudra@yahoo.com>
2023-04-19 18:45:55 -0700

    Added podDet to React

---
+++ pup-support/src/pages/PodcastDetails.js
@@ -0,0 +1,39 @@
+import pupsupportStressed from '...png';
+import { Link } from 'react-router-dom';
+
+/**
+ * Pupsupport home resource page 
+ */
+function PodcastDetails() {
+    // Related functions here
+
+    // HTML/React components here to render page
+    return (
+        <div>
+            {/* Section 1 */}
+            <section className="top">
+                <div className="container">
+                    <h2>Podcast Name</h2>
+                    <p>With A & B</p>
+
+                    {/* attach a playable audio file, idk how for now */}
+
+                    <img src={pupsupportStressed} />
+                </div>
+            </section>
+            
+            {/* Section 2 */}
+            <section className='details'>
+                <div className="container">
+                    <h3>Description</h3>
+                    <p>Lorem ....</p>
+
+                    <h3>Script</h3>
+                    <p>Lorem...</p>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default PodcastDetails;
