068dc2c89063942cf6f9dd0f87aeccafc0f00ded
Jaela Field <jaelaf@uw.edu>
2023-05-23 17:53:02 -0700

    little reorg

---
+++ coral-fly/schemas/team-member.ts
@@ -0,0 +1,13 @@
+export default {
+    name: 'member',
+    type: 'object',
+    fields: [
+        {
+            name: 'string',
+            position: 'string',
+            img: Image,
+        }
+    ]
+
+
+}
